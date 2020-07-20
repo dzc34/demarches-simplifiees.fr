6bdc2caf23928330cab8e92d711d081c022af7ff
Pierre de La Morinerie <kemenaran@gmail.com>
2020-07-20 16:42:01 +0200

    models: require belong_to associations on exercice

--- app/models/exercice.rb
+++ app/models/exercice.rb
@@ -1,5 +1,5 @@
 class Exercice < ApplicationRecord
-  belongs_to :etablissement
+  belongs_to :etablissement, optional: false
 
   validates :ca, presence: true, allow_blank: false, allow_nil: false
 end
