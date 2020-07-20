1b671b1396bafce9035261ee93fe396f6b969fea
Pierre de La Morinerie <kemenaran@gmail.com>
2020-07-20 14:06:03 +0000

    models: explicitly mark optional `belongs_to` relationship as so
    This prepares making the `belongs_to` relationship required by default.

--- app/models/france_connect_information.rb
+++ app/models/france_connect_information.rb
@@ -1,5 +1,5 @@
 class FranceConnectInformation < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :france_connect_particulier_id, presence: true, allow_blank: false, allow_nil: false
 end
